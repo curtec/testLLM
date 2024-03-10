045435de936cf7f0171f0bd660f6c7b3e149d1b0
Adam <adam@curtec.com.au>
2024-03-10 17:53:22 +1000

    app.py added

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ pipe = pipeline(task="summarization", model="Falconsai/text_summarization")
 st.header("Summerise your text")
 text = st.text_area("Paste your text here")
 
-if st.button("Summarize"):
-    with st.spinner("Summarizing your text"):
-        summary = pipe(text, max_length=100, min_length=30, do_sample=False)
+if st.button("Summarise"):
+    with st.spinner("Summarising your text"):
+        summary = pipe(text, max_length=50, min_length=3, do_sample=False)
         st.write(summary[0]['summary_text'])
