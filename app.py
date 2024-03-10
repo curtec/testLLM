863c4750c1fa51b935071eafa2b71970ab727bbf
Adam <adam@curtec.com.au>
2024-03-10 19:21:43 +1000

    app.py added

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 import streamlit as st
+from dotenv import load_dotenv
 from transformers import pipeline
 import torch
 
-
+load_dotenv()
 pipe = pipeline(task="summarization", model="Falconsai/text_summarization",
                       torch_dtype=torch.bfloat16)
 
@@ -11,5 +12,5 @@ text = st.text_area("Paste your text here")
 
 if st.button("Summarise"):
     with st.spinner("Summarising your text"):
-        summary = pipe(text, max_length=50, min_length=3, do_sample=False)
+        summary = pipe(text, max_length=200, min_length=50, do_sample=False)
         st.write(summary[0]['summary_text'])
